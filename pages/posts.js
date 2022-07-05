08311a74748737bffad0f3cd64e03987a1eaa280
lpigas <igornixdn@gmail.com>
2022-07-05 16:31:33 +0300

    new test

--- pages/posts.js
+++ pages/posts.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout/Layout";
 export default function posts() {
   return (
     <Layout>
-      <h1>
+      <h1 className="text-center text-4xl">
         <Link href="/posts/first-post">to first post</Link>
       </h1>
     </Layout>
