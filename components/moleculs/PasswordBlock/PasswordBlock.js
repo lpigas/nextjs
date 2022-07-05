8b5f3a0d49ff9436d0ef1500f87d7e78740511fa
lpigas <igornixdn@gmail.com>
2022-07-05 15:05:43 +0300

    add new page full orders, add password at page userData

--- components/moleculs/PasswordBlock/PasswordBlock.js
+++ components/moleculs/PasswordBlock/PasswordBlock.js
@@ -1,13 +1,20 @@
 import React from "react";
 import MyButton from "../../atoms/Buttons/MyButton/MyButton";
+import { useRouter } from "next/router";
 
-export default function PasswordBlock({ dataInput, setDataInput, onClick }) {
+export default function PasswordBlock({
+  dataInput,
+  setDataInput,
+  onClick,
+  onClickBack,
+}) {
+  const router = useRouter;
   return (
-    <div className="bg-white p-4 rounded-xl flex justify-center flex-col">
-      <div>
+    <div className="bg-white rounded-xl flex justify-center flex-col">
+      <div className="text-center">
         <input
           type="text"
-          className="border-2 border-black w-80 mb-3"
+          className="border-2 border-black w-80 m-3"
           value={dataInput.login}
           onChange={(e) =>
             setDataInput({ ...dataInput, login: e.target.value })
@@ -15,26 +22,22 @@ export default function PasswordBlock({ dataInput, setDataInput, onClick }) {
           placeholder={"Enter your login"}
         ></input>{" "}
       </div>
-      <div>
+      <div className="text-center">
         <input
           type="password"
-          className="border-2 border-black w-80"
+          className="border-2 border-black w-80 m-3"
           value={dataInput.password}
           onChange={(e) =>
             setDataInput({ ...dataInput, password: e.target.value })
           }
           placeholder={"Enter your password"}
         ></input>{" "}
-        <div className="m-1 flex justify-between">
+        <div className="m-1 flex justify-end">
           <MyButton size="lg" color="danger" onClick={onClick}>
             LogIn
           </MyButton>
-          <MyButton
-            size="lg"
-            color="danger"
-            onClick={() => setDataInput({ login: "", password: "" })}
-          >
-            Reset
+          <MyButton size="lg" color="danger" onClick={onClickBack}>
+            Back
           </MyButton>
         </div>
       </div>
